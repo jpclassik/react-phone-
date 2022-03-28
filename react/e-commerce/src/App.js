919856feb86927e548112f2954877ad6be78bbda
Jpclassik <ordjp54@gmail.com>
2022-03-28 09:28:23 -0700

    my first commit

---
+++ react/e-commerce/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
+import Modal from './components/Modal';
+
+
+
+function App() {
+  return (
+   <React.Fragment>
+     <Navbar/>
+     <Routes>
+       <Route path='/' element={<ProductList/>} />
+       <Route path='/details' element={<Details/>} />
+       <Route path='/cart' element={<Cart/>} />
+       <Route exact path='*' element={<Default/>} />
+  </Routes>
+  <Modal />
+   </React.Fragment>
+  );
+}
+
+export default App;
